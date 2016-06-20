c6780850e4d24043d529a9ee8c1b8e1bbfe1975b
Dhrubojyoti Das <dhrubojyoti_das@yahoo.in>
2016-06-20 11:56:04 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+// The files we want to cache
+var urlsToCache = [
+  '/',
+  '/style.css',
+  '/script.js'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
